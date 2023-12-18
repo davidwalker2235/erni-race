63c952f11758de81aabd9fa0c7e8ea285a37f536
david <davidwalker2235@gmail.com>
2023-12-18 17:01:41 +0100

    install net 2

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = (phase, { defaultConfig }) => {
+    return {
+        ...defaultConfig,
+
+        webpack: (config) => {
+            config.resolve = {
+                ...config.resolve,
+                fallback: {
+                    "fs": false,
+                    "path": false,
+                    "os": false,
+                    "net": false,
+                    "tls": false
+                }
+            }
+            return config
+        },
+    }
+}
