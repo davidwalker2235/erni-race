372b3a93ff7798e6f9f49ff0d4a5b006a56eb283
david <davidwalker2235@gmail.com>
2024-01-16 17:27:14 +0100

    add runner

--- src/app/cover.tsx
+++ src/app/cover.tsx
@@ -3,16 +3,17 @@
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import Runner from "@/app/game/runner";
-import React from "react";
+import React, {useState} from "react";
 
 export default function Cover() {
     const router = useRouter()
-
+    const [run, setRun] = useState(false)
     const navigateToGame = () => router.push('/game')
     return (
         <>
             <div style={{overflow: 'hidden'}} onClick={navigateToGame}>
-                <Runner active={false} image="/sprite.png"/>
+                <Runner active={run} image="/sprite.png"/>
+                <button onClick={() => setRun(true)}>play</button>
                 <div className="x-rays">
                     <Image id="rotating-image" src="/rays.png" alt="Rotating" width={3000} height={3000} />
                 </div>
--- src/app/game/runner.tsx
+++ src/app/game/runner.tsx
@@ -1,20 +1,32 @@
 "use client";
-import Spritesheet from 'react-responsive-spritesheet';
+import { Suspense, useState } from 'react'
+import { SpriteAnimator } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 
 const Runner = ({active, image}: {active: boolean, image: string}) => {
-
+    const [frameName, setFrameName] = useState('idle')
+    const onEnd = ({ currentFrameName, currentFrame }: any) => {
+        if (currentFrameName === 'celebration') {
+            setFrameName('idle')
+        }
+    }
     return (
-        <Spritesheet
-            style={{width: '100px', height: '100px'}}
-            image={image}
-            widthFrame={680}
-            heightFrame={470}
-            steps={8}
-            fps={6}
-            autoplay={active}
-            loop={true}
-            direction={`forward`}
-        />
+        <Canvas>
+            <Suspense fallback={null}>
+                <SpriteAnimator
+                    scale={[8,8,8]}
+                    position={[0,0,0]}
+                    startFrame={0}
+                    autoPlay={false}
+                    loop={true}
+                    numberOfFrames={8}
+                    alphaTest={0.01}
+                    textureImageURL={image}
+                    fps={8}
+                    play={active}
+                />
+            </Suspense>
+        </Canvas>
     )
 }
 
